ca3b7e4c916ff87ee021603aed645fb5c9239d8e
Yevgen Grytsay <yevgen_grytsay@mail.ru>
2016-09-14 22:03:20 +0300

    Example of mouse movement capturing

---
+++ mouse-move.elm
@@ -0,0 +1,62 @@
+import Html exposing (Html, div, text)
+import Html.App as App
+import Mouse exposing (..)
+import String
+
+
+main =
+  App.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+-- MODEL
+
+type alias Model = (Int, Int)
+
+
+init : (Model, Cmd Msg)
+init =
+  ((0, 0), Cmd.none)
+
+
+-- UPDATE
+
+type Msg
+  = Position Int Int
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Position x y ->
+      ((x, y), Cmd.none)
+
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.batch [
+    Mouse.moves (\{x, y} -> Position x y)
+  ]
+
+
+-- VIEW
+printModel : Model -> String
+printModel model =
+  String.concat [
+    "(",
+    toString (fst model),
+    "; ",
+    toString (snd model),
+    ")"
+  ]
+
+
+view : Model -> Html Msg
+view model =
+  div [] [text (printModel model)]
